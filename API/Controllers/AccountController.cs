206c9b3075399a2085f9601c653fc254cbba280b
ObradPR <obradpualic@gmail.com>
2023-10-18 17:48:20 +0200

    End of L435

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext context;
+        public AccountController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost("register")] // POST: /api/account/register ; we have route from BaseApiController
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            this.context.Users.Add(user);
+
+            await this.context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
