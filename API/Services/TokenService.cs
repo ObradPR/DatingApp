76e11b03ed5a786ddcdaab220a3321ef51674997
ObradPR <obradpualic@gmail.com>
2023-10-20 08:49:42 +0200

    End of L441

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
