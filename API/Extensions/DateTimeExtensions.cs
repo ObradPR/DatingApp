59ac4bb18cf05dd5717e2866fd3d16782e2ae3b9
ObradPR <obradpualic@gmail.com>
2023-11-26 11:48:42 +0100

    L8 V86

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalculateAge(this DateOnly dob)
+        {
+            DateOnly today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            int age = today.Year - dob.Year;
+
+            if(dob > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+}
